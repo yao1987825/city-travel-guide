59392a8a1b0c84bfbaace0cefb40d9c603cf238e
yao1987825 <yaojiwei5201@gmail.com>
2026-04-14 21:28:33 +0800

    Update weather_config.py with new QWEATHER_KEY

---
+++ weather_config.py
@@ -0,0 +1,5 @@
+QWEATHER_KEY = 'af81dcca52324aedbf38b840816c783f'
+
+# Existing content
+CITY_CODE_MAP = { 'New York': 'NY', 'Los Angeles': 'CA', 'Chicago': 'IL' }
+QWEATHER_BASE_URL = 'https://api.qweather.com/v7/weather/'
